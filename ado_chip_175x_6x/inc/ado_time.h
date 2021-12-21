4b3abaa5f24f0e9ca1eae9fb0600389c6a3a3dc8
RobertK66 <RobertK66@users.noreply.github.com>
2021-12-21 17:00:48 +0100

    remove complec time definitions from ado library -> moved to OBC application.

--- ado_chip_175x_6x/inc/ado_time.h
+++ ado_chip_175x_6x/inc/ado_time.h
@@ -10,25 +10,25 @@
 
 #include <chip.h>
 
-typedef uint32_t    ado_timestamp;
-typedef double      juliandayfraction;
-
-typedef struct ado_tim_tledatetime_s {
-    uint16_t            year;
-    juliandayfraction   dayOfYear;   // This starts with 1.0 on 00:00:00.0000 on 1st January and counts up to 365/6.xxxxx on 31.Dec 23:59:59.9999
-} ado_tim_tledatetime_t;
-
-typedef struct ado_tim_systemtime_s {
-    uint32_t                epochNumber;
-    ado_timestamp           msAfterStart;
-    ado_tim_tledatetime_t   utcOffset;      // year 0 meaning unknown/unsynced
-} ado_tim_systemtime_t;
-
-void TimeInit(uint32_t startOffsetMs, uint32_t epochNumber);
-void TimeSetUtc1(uint16_t year, uint8_t month, uint8_t dayOfMonth, uint8_t hour, uint8_t min, uint8_t sec);
-void TimeSetUtc2(RTC_TIME_T *fullTime);
-ado_timestamp TimeGetCurrentTimestamp();
-void TimeGetCurrentSystemTime(ado_tim_systemtime_t *sysTime);
-void TimeGetCurrentUtcTime(RTC_TIME_T *fullTime);
+//typedef uint32_t    ado_timestamp;
+//typedef double      juliandayfraction;
+//
+//typedef struct ado_tim_tledatetime_s {
+//    uint16_t            year;
+//    juliandayfraction   dayOfYear;   // This starts with 1.0 on 00:00:00.0000 on 1st January and counts up to 365/6.xxxxx on 31.Dec 23:59:59.9999
+//} ado_tim_tledatetime_t;
+//
+//typedef struct ado_tim_systemtime_s {
+//    uint32_t                epochNumber;
+//    ado_timestamp           msAfterStart;
+//    ado_tim_tledatetime_t   utcOffset;      // year 0 meaning unknown/unsynced
+//} ado_tim_systemtime_t;
+//
+//void TimeInit(uint32_t startOffsetMs, uint32_t epochNumber);
+//void TimeSetUtc1(uint16_t year, uint8_t month, uint8_t dayOfMonth, uint8_t hour, uint8_t min, uint8_t sec);
+//void TimeSetUtc2(RTC_TIME_T *fullTime);
+//ado_timestamp TimeGetCurrentTimestamp();
+//void TimeGetCurrentSystemTime(ado_tim_systemtime_t *sysTime);
+//void TimeGetCurrentUtcTime(RTC_TIME_T *fullTime);
 
 #endif /* MOD_ADO_TIME_H_ */
